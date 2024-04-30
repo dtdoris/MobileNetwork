5ef7444bf14a8eced11c07f8ac9375dc4bbf9e31
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2024-04-30 20:00:32 +0800

    - In progress: implementing PRR and BER calculation

--- LoRaReceiver/LoRaReceiver.ino
+++ LoRaReceiver/LoRaReceiver.ino
@@ -9,7 +9,7 @@ void setup()
     // When the power is turned on, a delay is required.
     delay(1500);
 
-    Serial.println("LoRa Receiver");s
+    Serial.println("LoRa Receiver");
 
     LoRa.setPins(RADIO_CS_PIN, RADIO_RST_PIN, RADIO_DIO0_PIN);
     if (!LoRa.begin(LoRa_frequency)) {
@@ -20,29 +20,63 @@ void setup()
 
 void loop()
 {
-  static unsigned int totalPackets = 0, receivedPackets = 0, bitErrors = 0;
+  static unsigned int receivedPackets = 0, bitErrors = 0;
     // try to parse packet
     int packetSize = LoRa.parsePacket();
     if (packetSize) {
-      // received a packet
-      receivedPackets++;
-      Serial.print("Received packet '");
-
+      
       String recv = "";
       // read packet
       while (LoRa.available()) {
           recv += (char)LoRa.read();
       }
-      totalPackets++;
-      Serial.println(recv);
-      Serial.println("RSSI: " + String(LoRa.packetRssi()) + "\tSNR: " + String(LoRa.packetSnr()));
-      // Periodically display PRR and BER
-      if (totalPackets % 50 == 0) { // Every 50 packets, display metrics
-        Serial.print("PRR: ");
-        Serial.print((float)receivedPackets / totalPackets * 100);
-        Serial.println("%");
-        // Serial.print("BER: ");
-        // Serial.println((float)bitErrors / (receivedPackets * expectedMsg.length() * 8));
+      if(recv.startsWith("4419grp6")) {
+        receivedPackets++;
+        String packet = recv.substring(prefix.length());
+        // Process the packet for BER and display the message without the prefix
+        processPacket(packet);
       }
     }
+}
+
+void processPacket(String received) {
+  String expectedOnesString = "11111111";
+
+  // NEEDS TO BE FIXED
+  // Periodically display PRR and BER. 
+  if (totalPackets % 50 == 0) { // Every 50 packets, display metrics
+    Serial.print("PRR: ");
+    Serial.print((float)receivedPackets / totalPackets * 100);
+    Serial.println("%");
+    // Serial.print("BER: ");
+    // Serial.println((float)bitErrors / (receivedPackets * expectedMsg.length() * 8));
+  }
+  // Check if the received message starts with the expected prefix
+  // Remove the prefix and print the rest of the message
+  String message = received.substring(expectedPrefix.length());
+  Serial.println("Received message: " + message);
+  Serial.print(message + ",");
+  Serial.print(millis() + ",");
+  Serial.print(String(LoRa.packetRssi()) + "," + String(LoRa.packetSnr())+ ",");
+
+  // Calculate and display BER
+  calculateBER(received.substring(0, expectedOnesString.length()), expectedOnesString);
+}
+
+void calculateBER(String receivedPrefix, String expectedPrefix) {
+  int errorBits = 0;
+  int totalBits = expectedPrefix.length() * 8; // 8 bits per character
+
+  for (int i = 0; i < expectedPrefix.length(); i++) {
+    byte receivedByte = receivedPrefix[i];
+    byte expectedByte = expectedPrefix[i];
+    for (int bit = 0; bit < 8; bit++) {
+      if (((receivedByte >> bit) & 0x01) != ((expectedByte >> bit) & 0x01)) {
+        errorBits++;
+      }
+    }
+  }
+
+  float ber = (float)errorBits / (float)totalBits;
+  Serial.println(ber, 6); // Print BER with high precision
 }
--- LoRaSender/LoRaSender.ino
+++ LoRaSender/LoRaSender.ino
@@ -1,57 +1,58 @@
 #include <LoRa.h>
-#include "boards.h"
-#define PACKET_HEADER "We are group 6 "
+#include <string>
+#include <iostream>
 
-#define SF 8      // ranged from 6-12, default is 7. Check docs.
-#define BW 125E3  // Bandwidth: 125 kHz
-#define TP 20     // Transmission Power (dBm)
-#define CR 5      // Coding Rate
+#include "boards.h"
+#define PACKET_HEADER "4419grp6"
+#define PACKET_MSG "[Ash] Do you like my pikachu hoodie?"
+
+int  SF = 8;
+long BW = 125E3;
+int  CR = 5;
+int  TP = 20;
 
 int counter = 0;
 
 void setup()
 {
+
   initBoard();
   // When the power is turned on, a delay is required.
   delay(1500);
 
   Serial.println("LoRa Sender");
   LoRa.setPins(RADIO_CS_PIN, RADIO_RST_PIN, RADIO_DIO0_PIN);
+
+  LoRa.setSpreadingFactor(SF);      // ranged from 6-12, default is 7. Check docs.
+  LoRa.setSignalBandwidth(BW);      // Bandwidth (eg.125E3 which is 125 kHz)
+  LoRa.setCodingRate4(CR);          // Coding Rate (eg. 4/5)
+  LoRa.setTxPower(TP);              // Transmission Power (dBm)
+  
   if (!LoRa.begin(LoRa_frequency)) {
-      Serial.println("LoRa init failed! Check your connections.");
+      Serial.println("Starting LoRa failed!");
       while (1);
   }
 
-  
-  LoRa.setSpreadingFactor(SF);     
-  LoRa.setSignalBandwidth(BW); 
-  LoRa.setCodingRate4(CR); // Coding Rate: 4/5
-  LoRa.setTxPower(TP);   
+
   Serial.println("LoRa init succeeded.");
 }
 
 void loop()
-{
-  Serial.print("Sending packet (ASH): Can someone lemme know that the sender works? :P (┬─┬ ノ( ゜-゜ノ)");
-  Serial.println(counter);
+{ 
+  // String sending_packet_msg = "Seq_no:" + String(counter) + " Sending packet: " + String(PACKET_MSG);
+  
+  String sending_packet_msg = String(counter) + "," + SF + "," + BW + "," + CR + "," + TP;
+
+  Serial.print("11111111"); // Fixed prefix
+  Serial.println(sending_packet_msg);
 
   // send packet
   LoRa.beginPacket();
   LoRa.print(PACKET_HEADER);
-  LoRa.print("Sending packet (ASH): Can someone lemme know that the sender works? :P (┬─┬ ノ( ゜-゜ノ)");
-  LoRa.print(counter);
+  LoRa.print("11111111"); // Fixed prefix
+  LoRa.print(sending_packet_msg);
   LoRa.endPacket();
-
-#ifdef HAS_DISPLAY
-  if (u8g2) {
-    char buf[256];
-    u8g2->clearBuffer();
-    u8g2->drawStr(0, 12, "Transmitting: OK!");
-    snprintf(buf, sizeof(buf), "Sending: %d", counter);
-    u8g2->drawStr(0, 30, buf);
-    u8g2->sendBuffer();
-  }
-#endif
+  
   counter++;
   delay(5000);
 }
